e708b03fb7f58640864adb950e19ddd64d1f62c7
Jeff Shoemaker <zsfoto@gmail.com>
2024-03-14 13:34:57 +0100

    False path in the composer.json Fixed DateTime behavior

--- src/Model/Behavior/DatepickerBehavior.php
+++ src/Model/Behavior/DatepickerBehavior.php
@@ -11,15 +11,14 @@ use Cake\Event\EventInterface;
 use ArrayObject;
 use Cake\Datasource\ConnectionManager;
 use Cake\I18n\FrozenDate;
+use Cake\I18n\FrozenTime;
 use Cake\I18n\I18n;
 
 class DatepickerBehavior extends Behavior
 {
-	
+
     public function beforeMarshal(EventInterface $event, ArrayObject $data, ArrayObject $options)
 	{	
-		$separator = '-';
-		
 		//$locale = Configure::read('App.defaultLocale');
 		$locale = I18n::getLocale();
 		$table = $event->getSubject()->getTable();
@@ -28,67 +27,37 @@ class DatepickerBehavior extends Behavior
 		$tables = $collection->listTables();
 		$tableSchema = $collection->describe($table);
 		$columns = $tableSchema->columns();
-		
 		//$name = $tableSchema->name();
 		
-		//foreach($columns as $c){			// Ebben benne van a created és a modified mező is.
 		foreach($data as $field => $value){	// Nincs benne a created és a modified mező. Hmm!
-		
-			//debug($field);
-			//debug($value);
-			//die();
-		
 			$type = $tableSchema->getColumnType($field);
-
-			if($type == 'date' || $type == 'time' || $type == 'datetime'){
-				$data[$field] = str_replace('/', '-', $data[$field]);
-				$data[$field] = str_replace('.', '-', $data[$field]);
-				if ($locale == 'pt_BR') {
-					list($d, $m, $y) = explode($separator, $data[$field]);
-				}
+			if(in_array($type, ['date', 'time', 'datetime']){
 				if ($locale == 'hu_HU') {
-					list($y, $m, $d) = explode($separator, $data[$field]);
+					switch( $type ){
+						case 'date': 
+							$data[$field] = FrozenDate::parseDate($data[$field], 'yyyy-MM-dd')->i18nFormat('yyyy-MM-dd');
+							break;
+						case 'time': 
+							$data[$field] = FrozenTime::parseTime($data[$field], 'HH:mm:ss')->i18nFormat('HH:mm:ss');
+							break;
+						case 'datetime':
+							$data[$field] = FrozenTime::parseDateTime($data[$field], 'yyyy-MM-dd HH:mm:ss')->i18nFormat('yyyy-MM-dd HH:mm:ss');
+							break;
+					}
 				}
+				
+				/*
 				if ($locale == 'en_GB') {
 					list($d, $m, $y) = explode($separator, $data[$field]);
 				}
 				if ($locale == 'en_US') {
 					list($d, $m, $y) = explode($separator, $data[$field]);
 				}
-				if ($locale == 'fr_FR') {
-					list($d, $m, $y) = explode($separator, $data[$field]);
-				}
-				if ($locale == 'hr_HR') {
-					list($d, $m, $y) = explode($separator, $data[$field]);
-				}
-				if ($locale == 'ru_RU') {
-					list($d, $m, $y) = explode($separator, $data[$field]);
-				}
-				if ($locale == 'ch_CH') {
-					list($d, $m, $y) = explode($separator, $data[$field]);
-				}
-				
-				$data[$field] = $y .'-'. $m .'-'. $d;
+				*/
 
-				switch( $type ){
-					case 'date': 
-						//$data[$field] = FrozenDate::parseDate( $data[$field] );
-						$data[$field] = FrozenDate::parse($data[$field])->i18nFormat('yyyy-MM-dd');
-						break;
-					case 'time': 
-						//$data[$field] = FrozenDate::parseTime( $data[$field] );
-						$data[$field] = FrozenDate::parse($data[$field])->i18nFormat('HH:ii:ss');
-						break;
-					case 'datetime':
-						//$data[$field] = FrozenDate::parseDateTime( $data[$field] );
-						$data[$field] = FrozenDate::parse($data[$field])->i18nFormat('yyyy-MM-dd HH:ii:ss');
-						break;
-				}
-				
-			}
-			
-		}
-
+			} // in_array types
+		}	// /.foreach
+		
 	}
-
+	
 }
